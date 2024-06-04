612d63e79484d8a75a39a81ef0dae99f78c5a3b2
Code-Master12 <121521567+Code-Master12@users.noreply.github.com>
2024-06-04 22:05:38 +0300

    Add files via upload

---
+++ pkn.lua
@@ -0,0 +1,32 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local OnPlayerKilledEvent
+local isActive = true
+
+local function findAndTrackOnPlayerKilled()
+    for _, item in ipairs(ReplicatedStorage:GetDescendants()) do
+        if item:IsA("RemoteEvent") and item.Name == "OnPlayerKilled" then
+            OnPlayerKilledEvent = item
+            print("OnPlayerKilled Remote Event Found")
+
+            OnPlayerKilledEvent.OnClientEvent:Connect(function(killed, killer)
+                if isActive then
+                    print("OnPlayerKilled Event Triggered")
+                    killerName = (killer and killer.Name or "Nil")
+                    killedName = (killed and killed.Name or "Nil")
+                    game:GetService("StarterGui"):SetCore("SendNotification", {
+                        Title = "Trime -S",
+                        Text = "Player killed!\nKiller: " .. killerName .. "\nKilled: " .. killedName,
+                    })
+                end
+            end)
+            break
+        end
+    end
+end
+
+findAndTrackOnPlayerKilled()
+
+while not OnPlayerKilledEvent do
+    findAndTrackOnPlayerKilled()
+    wait(5)
+end
