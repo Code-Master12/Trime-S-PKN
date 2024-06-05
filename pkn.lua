8eaf4092eb76c81460f1504d9784ebc049e44a37
Code-Master12 <121521567+Code-Master12@users.noreply.github.com>
2024-06-05 13:58:19 +0300

    Update pkn.lua

--- pkn.lua
+++ pkn.lua
@@ -1,5 +1,6 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local OnPlayerKilledEvent
+local isActive
 
 local function findAndTrackOnPlayerKilled()
     for _, item in ipairs(ReplicatedStorage:GetDescendants()) do
